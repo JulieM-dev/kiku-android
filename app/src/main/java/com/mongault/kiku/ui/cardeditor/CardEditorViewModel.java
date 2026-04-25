95e6e910a6f43a83ad66c33855e7fd93facb1f12
JulieM-dev <pixy.julie@gmail.com>
2026-04-25 10:21:35 +0200

    WIP submitNewCard

--- app/src/main/java/com/mongault/kiku/ui/cardeditor/CardEditorViewModel.java
+++ app/src/main/java/com/mongault/kiku/ui/cardeditor/CardEditorViewModel.java
@@ -12,36 +12,58 @@ import com.mongault.kiku.data.repository.RepositoryCallback;
 import com.mongault.kiku.model.Card;
 import com.mongault.kiku.model.CardReview;
 import com.mongault.kiku.model.Deck;
+import com.mongault.kiku.model.FormalityLevel;
 
 import java.util.List;
 
 public class CardEditorViewModel extends ViewModel {
 
     private final CardRepository cardRepository;
+    private final DeckRepository deckRepository;
 
-    private final MutableLiveData<Long> deckId = new MutableLiveData<>();
+    private final MutableLiveData<Deck> deck = new MutableLiveData<>();
     private final MutableLiveData<Card> card = new MutableLiveData<>();
+    private final MutableLiveData<String> japaneseText = new MutableLiveData<>();
+    private final MutableLiveData<String> translatedText = new MutableLiveData<>();
+    private final MutableLiveData<String> kanaText = new MutableLiveData<>();
+    private final MutableLiveData<String> romajiText = new MutableLiveData<>();
+    private final MutableLiveData<FormalityLevel> formalityLevel = new MutableLiveData<>();
     private final MutableLiveData<Boolean> isCardSaved = new MutableLiveData<>();
     private final MutableLiveData<String> error = new MutableLiveData<>();
     private final MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
 
-    public CardEditorViewModel (Long deckId) {
-        this.cardRepository = new CardRepository();
-        this.deckId.setValue(deckId);
-        this.isCardSaved.setValue(false);
-    }
 
     public CardEditorViewModel() {
         this.cardRepository = new CardRepository();
+        this.deckRepository = new DeckRepository();
         this.isCardSaved.setValue(false);
     }
 
-    public void submitNewCard(long deckId) {
+    public void loadDeck(long deckId) {
         isLoading.setValue(true);
-        cardRepository.createCard(deckId, card.getValue(), new RepositoryCallback<Card>() {
+        deckRepository.getDeck(deckId, new RepositoryCallback<Deck>() {
+            @Override
+            public void onSuccess(Deck data) {
+                deck.setValue(data);
+                isLoading.setValue(false);
+            }
+
+            @Override
+            public void onError(String errorMessage) {
+                error.setValue(errorMessage);
+                isLoading.setValue(false);
+            }
+        });
+    }
+
+    public void submitNewCard(Card card) {
+        isLoading.setValue(true);
+        Log.d("submitNewCard", "Submiting card : " + card.toString());
+        cardRepository.createCard(deck.getValue().getId(), card, new RepositoryCallback<Card>() {
             @Override
             public void onSuccess(Card card) {
                 isCardSaved.setValue(true);
+                isLoading.setValue(false);
                 Log.d("submit answer", "Submit answer a réussi");
             }
 
@@ -56,7 +78,9 @@ public class CardEditorViewModel extends ViewModel {
 
 
 
+
     public LiveData<Card> getCard() { return card; }
+    public LiveData<Deck> getDeck() { return deck; }
     public LiveData<String> getError() { return error; }
     public LiveData<Boolean> getIsLoading() { return isLoading; }
 }
